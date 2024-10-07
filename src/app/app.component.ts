b38cce72063381d7281e2e533fd4f8545494d790
pmtutor <projectmanager@apperr.com>
2024-10-07 21:36:18 +0530

    added: scrollbar css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { FooterComponent } from "./components/footer/footer.component";
@@ -10,6 +10,29 @@ import { FooterComponent } from "./components/footer/footer.component";
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'chopper';
+  constructor(private renderer: Renderer2) {
+    
+  }
+  ngOnInit(): void {
+    if (!this.isWebKitSupported()) {
+      // Apply non-WebKit scrollbar styles
+      console.log("Non-WebKit scrollbar styles applied");
+      const htmlTag = document.documentElement;
+      this.renderer.setStyle(htmlTag, 'scrollbar-width', 'thin');
+      this.renderer.setStyle(htmlTag, 'scrollbar-color', 'var(--theme-color) transparent');
+    }
+  }
+  isWebKitSupported(): boolean {
+    const userAgent = navigator.userAgent;
+
+    // Check for WebKit engine (including Safari, older Chrome)
+    const isWebKit = /AppleWebKit/.test(userAgent);
+    
+    // Check for Blink engine (used by newer versions of Chrome)
+    const isBlink = /Chrome/.test(userAgent) && /Google Inc/.test(navigator.vendor);
+
+    return isWebKit || isBlink;
+  }
 }
